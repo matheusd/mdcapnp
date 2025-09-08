604e342b332675ef5bbd2a57a31451d6308acb21
Matheus Degiovani <opensource@matheusd.com>
2025-09-08 10:22:54 -0300

    sigvalue: Add Stateful

---
+++ internal/sigvalue/stateful.go
@@ -0,0 +1,210 @@
+// Copyright (c) 2025 Matheus Degiovani. All rights reserved.
+// Use of this source code is governed by a source-available
+// license that can be found in the LICENSE file.
+
+package sigvalue
+
+import (
+	"cmp"
+	"context"
+	"sync"
+)
+
+// StateType is the type of value that can be used as a state of a [Stateful]
+// instance.
+//
+// It must be an ordered type to allow the [Stateful.AtLeast] and similar
+// functions to work.
+//
+// No semantics are attached to the state itself: that is left to client code to
+// define.
+type StateType = cmp.Ordered
+
+type stateTargetComparator[S StateType] func(state, target S) bool
+
+func stateAtLeast[S StateType](state, target S) bool {
+	return state >= target
+}
+
+func stateNotGreater[S StateType](state, target S) bool {
+	return state <= target
+}
+
+func stateExactly[S StateType](state, target S) bool {
+	return state == target
+}
+
+type stateChangeEvent[S StateType, V any] struct {
+	state S
+	value V
+}
+
+type stateChangeWaiterType int
+
+const (
+	waitExactly stateChangeWaiterType = iota
+	waitAtLeast
+	waitNotGreater
+)
+
+type stateChangeWaiter[S StateType, V any] struct {
+	c  chan stateChangeEvent[S, V]
+	wt stateChangeWaiterType
+
+	targetState S
+}
+
+// Stateful is a signalled value that has a "current state" and value bound to
+// that state. It allows client code to wait for and react to specific state
+// changes, effected by other goroutines.
+//
+// Values must not be copied after initialization and are safe for use from
+// multiple goroutines.
+//
+// An empty stateful value of a given type is ready for use, with "current
+// state" and "current value" set to their corresponding empty values.
+type Stateful[S StateType, V any] struct {
+	mu sync.Mutex
+
+	state   S
+	value   V
+	waiters []stateChangeWaiter[S, V]
+}
+
+// NewStateful creates a new Stateful value initialized to the passed initial
+// state and value.
+func NewStateful[S StateType, V any](initialState S, initialValue V) *Stateful[S, V] {
+	return &Stateful[S, V]{state: initialState, value: initialValue}
+}
+
+func (s *Stateful[S, V]) matchesType(state, target S, wt stateChangeWaiterType) bool {
+	switch wt {
+	case waitExactly:
+		return stateExactly(state, target)
+	case waitAtLeast:
+		return stateAtLeast(state, target)
+	case waitNotGreater:
+		return stateNotGreater(state, target)
+	default:
+		panic("missing case")
+	}
+}
+
+// checkAndCallWaiters checks which waiters are fulfilled by the current state
+// and calls them.
+func (s *Stateful[S, V]) checkAndCallWaiters() {
+	if len(s.waiters) == 0 {
+		return
+	}
+
+	event := stateChangeEvent[S, V]{state: s.state, value: s.value}
+
+	// Determine which waiters (if any) to signal and remove them.
+	for i := 0; i < len(s.waiters); {
+		w := &s.waiters[i]
+		if !s.matchesType(s.state, w.targetState, w.wt) {
+			i++
+			continue
+		}
+
+		// Signal the channel. Note this is done under the s.mu.Lock(),
+		// therefore w.c MUST be buffered to avoid deadlocking.
+		w.c <- event
+		s.removeWaiterWithChan(w.c)
+	}
+}
+
+// Set sets the state and value of the current state. Any waiters that match
+// this new state will be alerted to it.
+func (s *Stateful[S, V]) Set(newState S, newValue V) (oldState S, oldValue V) {
+	s.mu.Lock()
+	oldState, oldValue = s.state, s.value
+	s.state, s.value = newState, newValue
+	s.checkAndCallWaiters()
+	s.mu.Unlock()
+	return
+}
+
+// Modify modifies the current state according to the given function. Any errors
+// returned by f are bubbled up to the caller.
+//
+// NOTE: this is called with the Stateful locked, so f should not block for
+// long.
+func (s *Stateful[S, V]) Modify(f func(oldState S, oldValue V) (newState S, newValue V, err error)) (err error) {
+	s.mu.Lock()
+	s.state, s.value, err = f(s.state, s.value)
+	s.checkAndCallWaiters()
+	s.mu.Unlock()
+	return
+}
+
+func (s *Stateful[S, V]) addWaiter() *stateChangeWaiter[S, V] {
+	c := make(chan stateChangeEvent[S, V], 1) // Channel MUST be buffered.
+	s.waiters = append(s.waiters, stateChangeWaiter[S, V]{c: c})
+	return &s.waiters[len(s.waiters)-1]
+}
+
+func (s *Stateful[S, V]) removeWaiterWithChan(c chan stateChangeEvent[S, V]) {
+	for i := range s.waiters {
+		if s.waiters[i].c != c {
+			continue
+		}
+
+		lw := len(s.waiters)
+		if i < lw-1 {
+			s.waiters[i] = s.waiters[lw-1]
+		}
+		s.waiters[lw-1] = stateChangeWaiter[S, V]{}
+		s.waiters = s.waiters[:lw-1]
+		break
+	}
+}
+
+func (s *Stateful[S, V]) waitStateFunc(ctx context.Context, targetState S, wt stateChangeWaiterType) (state S, value V, err error) {
+	var c chan stateChangeEvent[S, V]
+
+	s.mu.Lock()
+	if s.matchesType(s.state, targetState, wt) {
+		state, value = s.state, s.value
+	} else {
+		w := s.addWaiter()
+		w.targetState = targetState
+		c = w.c
+	}
+	s.mu.Unlock()
+
+	if c != nil {
+		select {
+		case e := <-c:
+			state, value = e.state, e.value
+		case <-ctx.Done():
+			err = context.Cause(ctx)
+			s.mu.Lock()
+			s.removeWaiterWithChan(c)
+			s.mu.Unlock()
+		}
+	}
+
+	return
+}
+
+// WaitStateAtLeast waits until the state is changed to be at least (>=) the
+// passed target state or the context is canceled. It returns immediately if the
+// current state already matches this requirement.
+func (s *Stateful[S, V]) WaitStateAtLeast(ctx context.Context, targetState S) (state S, value V, err error) {
+	return s.waitStateFunc(ctx, targetState, waitAtLeast)
+}
+
+// WaitStateNotGreater waits until the state is changed to be no greater (<=)
+// than the passed target state or the context is canceled. It returns
+// immediately if the current state already matches this requirement.
+func (s *Stateful[S, V]) WaitStateNotGreater(ctx context.Context, targetState S) (state S, value V, err error) {
+	return s.waitStateFunc(ctx, targetState, waitNotGreater)
+}
+
+// WaitStateExactly waits until the state is changed to be exactly (==) equal to
+// the passed target state or the context is canceled. It returns immediately if
+// the current state already matches this requirement.
+func (s *Stateful[S, V]) WaitStateExactly(ctx context.Context, targetState S) (state S, value V, err error) {
+	return s.waitStateFunc(ctx, targetState, waitExactly)
+}
---
+++ internal/sigvalue/stateful_test.go
@@ -0,0 +1,234 @@
+// Copyright (c) 2025 Matheus Degiovani. All rights reserved.
+// Use of this source code is governed by a source-available
+// license that can be found in the LICENSE file.
+
+package sigvalue
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"matheusd.com/testctx"
+)
+
+// TestStatefulWait tests the various Wait() functions of the Stateful signalled
+// value.
+func TestStatefulWait(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		f       string
+		is      int32 // Initial state
+		ts      int32 // Target state
+		ss      int32 // Set() state
+		want    bool
+		skipSet bool
+	}{
+		{f: "atLeast", is: 0, ts: 0, ss: 0, want: true},
+		{f: "atLeast", is: 0, ts: 10, ss: 0, want: false},
+		{f: "atLeast", is: 0, ts: 10, ss: -1, want: false},
+		{f: "atLeast", is: 0, ts: 10, ss: +1, want: false},
+		{f: "atLeast", is: 0, ts: 10, ss: 9, want: false},
+		{f: "atLeast", is: 0, ts: 10, ss: 10, want: true},
+		{f: "atLeast", is: 0, ts: 10, ss: 20, want: true},
+		{f: "atLeast", is: 100, ts: 200, ss: 201, want: true},
+		{f: "atLeast", is: 100, ts: 200, ss: 199, want: false},
+		{f: "atLeast", is: 100, ts: 200, skipSet: true, want: false},
+		{f: "atLeast", is: 100, ts: 100, skipSet: true, want: true},
+		{f: "atLeast", is: 100, ts: 90, skipSet: true, want: true},
+
+		{f: "notGreater", is: 0, ts: 0, ss: 0, want: true},
+		{f: "notGreater", is: 0, ts: 10, ss: 0, want: true},
+		{f: "notGreater", is: 0, ts: 10, ss: -1, want: true},
+		{f: "notGreater", is: 0, ts: 10, ss: +1, want: true},
+		{f: "notGreater", is: 0, ts: 10, ss: 9, want: true},
+		{f: "notGreater", is: 0, ts: 10, ss: 10, want: true},
+		{f: "notGreater", is: 0, ts: 10, ss: 20, want: false},
+		{f: "notGreater", is: 100, ts: 200, ss: 201, want: false},
+		{f: "notGreater", is: 100, ts: 200, ss: 199, want: true},
+
+		{f: "exactly", is: 0, ts: 0, ss: 0, want: true},
+		{f: "exactly", is: 0, ts: 10, ss: 10, want: true},
+		{f: "exactly", is: 0, ts: 0, ss: 9, want: false},
+		{f: "exactly", is: 0, ts: 0, ss: 11, want: false},
+		{f: "exactly", is: 100, ts: 200, ss: 199, want: false},
+		{f: "exactly", is: 100, ts: 200, ss: 200, want: true},
+		{f: "exactly", is: 100, ts: 200, ss: 201, want: false},
+		{f: "exactly", is: 0, ts: 0, skipSet: true, want: true},
+		{f: "exactly", is: 100, ts: 200, skipSet: true, want: false},
+		{f: "exactly", is: 100, ts: 99, skipSet: true, want: false},
+		{f: "exactly", is: 100, ts: 100, skipSet: true, want: true},
+		{f: "exactly", is: 100, ts: 101, skipSet: true, want: false},
+	}
+
+	const initVal = "initial"
+	const setVal = "set"
+
+	for _, tc := range tests {
+		tc := tc
+		setStr := fmt.Sprintf("set %d", tc.ss)
+		if tc.skipSet {
+			setStr = "skip set"
+		}
+		name := fmt.Sprintf("from %d %s wait %s(%d)", tc.is, setStr, tc.f, tc.ts)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			st := NewStateful(tc.is, initVal)
+			var f func(ctx context.Context) (string, error)
+			switch tc.f {
+			case "atLeast":
+				f = func(ctx context.Context) (string, error) {
+					_, v, err := st.WaitStateAtLeast(ctx, tc.ts)
+					return v, err
+				}
+			case "notGreater":
+				f = func(ctx context.Context) (string, error) {
+					_, v, err := st.WaitStateNotGreater(ctx, tc.ts)
+					return v, err
+				}
+			case "exactly":
+				f = func(ctx context.Context) (string, error) {
+					_, v, err := st.WaitStateExactly(ctx, tc.ts)
+					return v, err
+				}
+			default:
+				panic("implement f")
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+			defer cancel()
+
+			vChan := make(chan string, 1)
+			errChan := make(chan error, 1)
+			go func() {
+				v, err := f(ctx)
+				if err != nil {
+					errChan <- err
+				} else {
+					vChan <- v
+				}
+			}()
+
+			want := initVal
+			if !tc.skipSet {
+				st.Set(tc.ss, setVal)
+				want = setVal
+			}
+
+			select {
+			case err := <-errChan:
+				if tc.want {
+					t.Fatalf("Received error %v when expected result", err)
+				}
+			case got := <-vChan:
+				if !tc.want {
+					t.Fatalf("Got value %s when expected error", got)
+				}
+				if got != want {
+					t.Fatalf("Unexpected value: got %s, want %s", got, want)
+				}
+			}
+		})
+	}
+}
+
+// BenchmarkStatefulSet benchmarks setting values in a Stateful under various
+// circumstances.
+func BenchmarkStatefulSet(b *testing.B) {
+	b.Run("no waiters", func(b *testing.B) {
+		st := NewStateful(0, 0)
+
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := range b.N {
+			st.Set(i, i)
+		}
+	})
+
+	b.Run("one waiter always match", func(b *testing.B) {
+		// Make a channel big enough to receive every event without
+		// needing a new goroutine to process.
+		c := make(chan stateChangeEvent[int, int], b.N)
+		waiters := []stateChangeWaiter[int, int]{{}}
+
+		st := NewStateful(0, 0)
+
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := range b.N {
+			// Manually set the waiters on every iteration.
+			waiters[0] = stateChangeWaiter[int, int]{
+				wt:          waitAtLeast,
+				targetState: -1,
+				c:           c,
+			}
+			st.waiters = waiters
+			st.Set(i, i)
+		}
+	})
+
+	b.Run("one waiter never match", func(b *testing.B) {
+		// Make a channel big enough to receive every event without
+		// needing a new goroutine to process.
+		c := make(chan stateChangeEvent[int, int], b.N)
+		waiters := []stateChangeWaiter[int, int]{{}}
+
+		st := NewStateful(0, 0)
+
+		b.ReportAllocs()
+		b.ResetTimer()
+		for i := range b.N {
+			// Manually set the waiters on every iteration.
+			waiters[0] = stateChangeWaiter[int, int]{
+				wt:          waitNotGreater,
+				targetState: -1,
+				c:           c,
+			}
+			st.waiters = waiters
+			st.Set(i, i)
+		}
+	})
+}
+
+// BenchmarkStatefulWait benchmarks waiting for a Stateful change in various
+// ways.
+func BenchmarkStatefulWait(b *testing.B) {
+	b.Run("already set", func(b *testing.B) {
+		st := NewStateful(0, 0)
+		ctx := testctx.New(b)
+
+		b.ReportAllocs()
+		b.ResetTimer()
+
+		for range b.N {
+			_, _, err := st.WaitStateAtLeast(ctx, 0)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+	// Uses a context that is already done to ensure we go through the
+	// entire function without having to create a goroutine.
+	b.Run("context done", func(b *testing.B) {
+		st := NewStateful(0, 0)
+		targetErr := errors.New("guard error")
+		ctx, cancel := context.WithCancelCause(context.Background())
+		cancel(targetErr)
+
+		b.ReportAllocs()
+		b.ResetTimer()
+
+		for range b.N {
+			_, _, err := st.WaitStateAtLeast(ctx, 1)
+			if !errors.Is(err, targetErr) {
+				b.Fatal(err)
+			}
+		}
+
+	})
+}
